57ce6247347af38ce688f782885cc1daf9528782
neword <newordn@gmail.com>
2018-01-25 13:59:48 +0100

    Update show.blade.php

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,7 +6,7 @@
 @foreach($students as $student)
 <div class="card col s2" style="margin-right: 10px;height: 300px;"><strong class="btn">NOM/SURNAME</strong> {{$student->last_name}}<br/><strong class="btn">PRÉNOM/FIRST NAME</strong>
 {{$student->first_name}}<br/><strong class="btn">MATRICULE </strong>{{$student->matricule}}<br/>
-<strong class="btn">ASSIDUITÉE</strong><br/>{{count($student->absences)}}<br/>
+<strong class="btn">ABSENCES</strong><br/>{{count($student->absences)}}<br/>
 <a class="btn blue" href="/bulletin/{{$student->id}}"><i class="fa fa-address-card-o"></i></a><a href="/absence/{{$student->id}}" class="btn brown"><i class="fa fa-eye"></i></a></div>
 @endforeach
 </div>
